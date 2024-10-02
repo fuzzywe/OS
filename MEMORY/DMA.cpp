833950d1f60be4921de50ab5d387fe15893faf01
JANANI ASOKAN <chicatechhub@gmail.com>
2024-10-02 13:40:52 +0530

    Create DMA.cpp

---
+++ MEMORY/DMA.cpp
@@ -0,0 +1,4 @@
+<code>Direct Memory Access in OS - GeeksforGeeksDirect memory access (DMA) is a computer feature that allows hardware to access the main memory of a computer without the central processing unit (CPU). This allows the CPU to focus on other tasks, which can improve overall computer performance. 
+
+How it works
+DMA uses a dedicated controller to transfer data between a device and memory, bypassing the CPU</code>
